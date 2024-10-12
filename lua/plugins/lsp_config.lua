b3f16d03137f92a2d62544163a577ce738268c19
it-a-me <wobbe.carter@gmail.com>
2024-10-12 14:16:27 -0700

    lspconfig: Add support for deno

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -163,7 +163,7 @@ return { -- LSP Configuration & Plugins
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
-      'stylua',   -- Used to format Lua code
+      'stylua', -- Used to format Lua code
     })
     --      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -179,5 +179,6 @@ return { -- LSP Configuration & Plugins
         end,
       },
     }
+    require 'lspconfig'.denols.setup {}
   end,
 }
