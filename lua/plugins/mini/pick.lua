d0e7714432665728aa447c3b943ba4cc70fc2f87
Carter Wobbe <wobbe.carter@gmail.com>
2025-03-10 16:45:53 -0700

    refactor(mini): Refactor workspace_root into a new function

--- lua/plugins/mini/pick.lua
+++ lua/plugins/mini/pick.lua
@@ -1,14 +1,22 @@
 require 'mini.pick'.setup {}
-vim.keymap.set('n', '<leader>f', function()
+
+local ws_root = function()
   if MiniGit.get_buf_data(0) ~= nil then
-    MiniPick.builtin.files({}, { source = { cwd = MiniGit.get_buf_data(0).root } })
+    return { source = { cwd = MiniGit.get_buf_data(0).root } }
   else
-    MiniPick.builtin.files()
+    return {}
   end
-end)
+end
 
 vim.keymap.set('n', '<leader>b', MiniPick.builtin.buffers, { desc = "Open Buffers" })
 vim.keymap.set('n', '<leader>g', MiniPick.builtin.grep_live, { desc = "Live Grep" })
+
+vim.keymap.set('n', '<leader>f',
+  function()
+    MiniPick.builtin.files({}, ws_root())
+  end,
+  { desc = 'Find Files' })
+
 
 vim.keymap.set('n', '<leader>hh', MiniPick.builtin.help, { desc = 'Help' })
 vim.keymap.set('n', '<leader>hc', MiniExtra.pickers.commands, { desc = 'Commands' })
