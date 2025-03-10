24e1505e1fb07ddb8d4e1bbec9e7b9bc19e4d1b8
Carter Wobbe <wobbe.carter@gmail.com>
2025-03-10 16:35:36 -0700

    feat(lsp): Add keybinds for goto declaration/definition

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -12,6 +12,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
       return
     end
 
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = args.buf })
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = args.buf })
+
     if client:supports_method('textDocument/completion') then
       -- Enable auto-completion
       vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
