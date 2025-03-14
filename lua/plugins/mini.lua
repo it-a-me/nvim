678f49a172f0acabe74558c0a459e65a61ced0c1
Carter Wobbe <wobbe.carter@gmail.com>
2025-03-14 11:38:25 -0700

    feat(mini): Add git to statuscolumn via mini.diff

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -7,6 +7,16 @@ return {
     require 'mini.files'.setup {}
     vim.keymap.set('n', '<leader>e', MiniFiles.open, { desc = "Open File Explorer" })
 
+    require 'mini.diff'.setup {
+      view = {
+        style = 'sign',
+        signs = {
+          add = '+',
+          change = '~',
+          delete = '-' },
+      }
+    }
+
     require 'mini.git'.setup {}
 
     require 'mini.extra'.setup {}
