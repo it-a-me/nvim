2d44674eafd00cd62f7396f4441aa41bdc0c6d0a
Carter Wobbe <wobbe.carter@gmail.com>
2025-04-15 12:35:13 -0700

    fixup! refactor(lsp): Update lsp setup to use vim.lsp.config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,7 +3,7 @@ local mason = {
   dependencies = {
     { 'williamboman/mason.nvim', config = {} }
   },
-  config = {
+  opts = {
     ensure_installed = {},
     handlers = {
       function(server_name) -- default handler
@@ -18,7 +18,7 @@ return {
   dependencies = {
     mason
   },
-  init = function()
+  config = function()
     for name, _ in vim.fs.dir(vim.fn.stdpath('config') .. '/lua/lsp_overrides') do
       if #name > 4 and string.sub(name, -4) == '.lua' then
         local lsp_server = string.sub(name, 0, -5)
