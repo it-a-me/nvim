f4812fc63952178f90616c02aef44671af7e173e
Carter Wobbe <wobbe.carter@gmail.com>
2025-06-12 12:39:59 -0700

    feat(dap): Add debugger support

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,9 @@
+return {
+  "rcarriga/nvim-dap-ui",
+  dependencies = {
+    { "mfussenegger/nvim-dap" },
+    { "nvim-neotest/nvim-nio" },
+    { "leoluz/nvim-dap-go",   config = {} }
+  },
+  config = {}
+}
