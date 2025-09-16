58b7cc1c58ac695a32378fe6dde2b12ceb235970
Carter Wobbe <wobbe.carter@gmail.com>
2025-09-15 23:13:44 -0700

    Move lsp options to after/lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,15 +18,4 @@ return {
   dependencies = {
     mason
   },
-  config = function()
-    for name, _ in vim.fs.dir(vim.fn.stdpath('config') .. '/lua/lsp_overrides') do
-      if #name > 4 and string.sub(name, -4) == '.lua' then
-        local lsp_server = string.sub(name, 0, -5)
-        vim.lsp.config(lsp_server,
-          require('lsp_overrides' .. '.' .. lsp_server))
-      end
-    end
-    vim.lsp.enable('rosyln')
-    vim.lsp.enable('vala_ls')
-  end
 }
