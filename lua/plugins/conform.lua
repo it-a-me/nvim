1111f79ecb22047c1695dbb4b3d1602bfb511412
Carter Wobbe <wobbe.carter@gmail.com>
2026-01-10 22:51:33 -0800

    Enable pandoc markdown formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,13 +1,10 @@
 return {
   'stevearc/conform.nvim',
-  lazy = true,
+  lazy = false,
   keys = {
     { "<M-f>", function() require 'conform'.format() end, desc = 'Format Buffer' }
   },
   opts = {
-    formatters_by_ft = {
-      markdown = { "pandoc" }
-    },
     default_format_opts = {
       lsp_format = "fallback",
     },
@@ -19,8 +16,16 @@ return {
           "--standalone", -- Support metadata blocks
           "--to", "markdown-simple_tables"
         },
-        inherit = false
+        stdin = true,
       }
-    }
-  }
+    },
+    formatters_by_ft = {
+      markdown = { "pandoc" }
+    },
+  },
+  config = function(_, opts)
+    local conform = require("conform")
+    -- This setup call registers the formatters defined in 'opts'
+    conform.setup(opts)
+  end,
 }
