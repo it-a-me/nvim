72705920dda6be205167b98427df7f663078930b
Carter Wobbe <wobbe.carter@gmail.com>
2026-04-19 16:22:42 -0700

    tree-sitter: Add d2 support

--- lua/plugins/tree_sitter.lua
+++ lua/plugins/tree_sitter.lua
@@ -1,11 +1,19 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  main = 'nvim-treesitter.configs',
-  opts = {
-    auto_install = true,
-    ensure_installed = { "lua", "vim", "vimdoc" },
-    highlight = {
-      enable = true
+  {
+    "ravsii/tree-sitter-d2",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    version = "*", -- use the latest git tag instead of main
+    build = "make nvim-install",
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    main = 'nvim-treesitter.configs',
+    opts = {
+      auto_install = true,
+      ensure_installed = { "lua", "vim", "vimdoc" },
+      highlight = {
+        enable = true
+      }
     }
   }
 }
